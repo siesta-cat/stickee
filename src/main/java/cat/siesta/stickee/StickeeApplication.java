e87116ae7e394d078dcdf549c6f3846df8eb5930
Sugui <anaisusg@gmail.com>
2024-08-01 18:17:50 +0200

    First commit

---
+++ src/main/java/cat/siesta/stickee/StickeeApplication.java
@@ -0,0 +1,13 @@
+package cat.siesta.stickee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StickeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StickeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cat/siesta/stickee/StickeeApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.siesta.stickee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StickeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
