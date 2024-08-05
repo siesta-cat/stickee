765c3a011bf172d179eee8558197032f11ede0bb
Sugui <anaisusg@gmail.com>
2024-08-05 17:17:26 +0200

    Added correct GenerationType to insert without specifying id

--- src/main/java/cat/siesta/stickee/Note.java
+++ src/main/java/cat/siesta/stickee/Note.java
@@ -3,12 +3,13 @@ package cat.siesta.stickee;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Note {    
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column(nullable = false)
