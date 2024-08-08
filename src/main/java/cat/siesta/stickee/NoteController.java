d5f8642b9e70eca4bc5e4f5abfd4ada5acdbd5a2
Sugui <anaisusg@gmail.com>
2024-08-08 18:50:06 +0200

    Changed note controller annotation to @RestController

--- src/main/java/cat/siesta/stickee/NoteController.java
+++ src/main/java/cat/siesta/stickee/NoteController.java
@@ -1,15 +1,17 @@
 package cat.siesta.stickee;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/")
 public class NoteController {
 
@@ -17,14 +19,16 @@ public class NoteController {
     private NoteService noteService;
 
     @GetMapping("/{resource-locator}")
-    public @ResponseBody String getNote(@PathVariable("resource-locator") String resourceLocator) {
+    public String getNote(@PathVariable("resource-locator") String resourceLocator) {
         var maybeNote = noteService.get(resourceLocator);
         return maybeNote.map(Note::getText).orElseThrow(NoteNotFoundException::new);
     }
 
     @PostMapping("/create")
-    public String postNote(@RequestParam("text") String text) {
+    public ResponseEntity<String> postNote(@RequestParam("text") String text) {
         var resourceLocator = noteService.create(new Note(text)).getResourceLocator().orElseThrow().toString();
-        return "redirect:/" + resourceLocator;
+        var headers = new HttpHeaders();
+        headers.add("Location", "/" + resourceLocator);
+        return new ResponseEntity<>(headers, HttpStatus.FOUND);
     }
 }
