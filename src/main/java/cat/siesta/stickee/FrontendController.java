254a28be0ac6dd7955a31f0a9a9f00785e6d79ab
Alie <bizcochito@anartist.org>
2024-08-11 18:51:26 +0200

    installed thymeleaf and tailwind

---
+++ src/main/java/cat/siesta/stickee/FrontendController.java
@@ -0,0 +1,14 @@
+package cat.siesta.stickee;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FrontendController {
+
+	@GetMapping("/")
+	public String home() {
+		return "index";
+	}
+
+}
