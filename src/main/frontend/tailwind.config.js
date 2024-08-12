8d635dfd7077988505ec79f17a08b5a360d88ca2
Sugui <anaisusg@gmail.com>
2024-08-12 18:29:44 +0200

    Added functional frontend page

--- src/main/frontend/tailwind.config.js
+++ src/main/frontend/tailwind.config.js
@@ -1,7 +1,13 @@
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["../resources/templates/**/*.{html,js}"],
   theme: {
+    colors: {
+      primary: colors.violet,
+      ...colors
+    },
     extend: {},
   },
   plugins: [],
