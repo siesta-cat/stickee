7c60eabe0e73da1f90da892fa96010012ed0acfd
Alie <bizcochito@anartist.org>
2024-08-12 17:46:58 +0200

    Add lombok for generating the getters and similar boilerplate for Note

--- src/main/java/cat/siesta/stickee/persistence/Note.java
+++ src/main/java/cat/siesta/stickee/persistence/Note.java
@@ -6,8 +6,14 @@ import java.util.Optional;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
 
 @Entity
+@ToString
+@EqualsAndHashCode
+@Getter
 public class Note {
 
     @Id
@@ -17,7 +23,7 @@ public class Note {
     private String text;
 
     @Column(nullable = false)
-    private LocalDateTime creationTimestamp = LocalDateTime.now();
+    private final LocalDateTime creationTimestamp = LocalDateTime.now();
 
     public Note() {
         this.text = "";
@@ -32,49 +38,7 @@ public class Note {
         this.text = text;
     }
 
-    public String getText() {
-        return text;
-    }
-
     public Optional<String> getResourceLocator() {
         return Optional.ofNullable(this.resourceLocator);
     }
-
-    @Override
-    public String toString() {
-        return "Note [text=" + text + ", resourceLocator=" + resourceLocator + "]";
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((resourceLocator == null) ? 0 : resourceLocator.hashCode());
-        result = prime * result + ((text == null) ? 0 : text.hashCode());
-        result = prime * result + ((creationTimestamp == null) ? 0 : creationTimestamp.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Note other = (Note) obj;
-        if (resourceLocator == null) {
-            if (other.resourceLocator != null)
-                return false;
-        } else if (!resourceLocator.equals(other.resourceLocator))
-            return false;
-        if (text == null) {
-            if (other.text != null)
-                return false;
-        } else if (!text.equals(other.text))
-            return false;
-        return true;
-    }
-
 }
