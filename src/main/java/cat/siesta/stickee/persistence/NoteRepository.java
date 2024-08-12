d30f65b7a2c0d2ed26c8ee63e908947e88cb5d5e
Alie <bizcochito@anartist.org>
2024-08-12 16:40:51 +0200

    fix key for note repository

--- src/main/java/cat/siesta/stickee/persistence/NoteRepository.java
+++ src/main/java/cat/siesta/stickee/persistence/NoteRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface NoteRepository extends JpaRepository<Note, Long> {
+public interface NoteRepository extends JpaRepository<Note, String> {
     public Optional<Note> findByResourceLocator(String resourceLocator);
     public boolean existsByResourceLocator(String resourceLocator);
 }
