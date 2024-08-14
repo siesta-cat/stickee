ef56624b0b182b9f738b20db66e94f60ae0b2dfa
Sugui <anaisusg@gmail.com>
2024-08-14 17:22:06 +0200

    Renamed stickee.yml to stickee.yaml

--- src/main/java/cat/siesta/stickee/config/StickeeConfiguration.java
+++ src/main/java/cat/siesta/stickee/config/StickeeConfiguration.java
@@ -9,7 +9,7 @@ import lombok.Setter;
 
 @Configuration
 @ConfigurationProperties
-@PropertySource("classpath:stickee.yml")
+@PropertySource("classpath:stickee.yaml")
 @Getter
 @Setter
 public class StickeeConfiguration {
