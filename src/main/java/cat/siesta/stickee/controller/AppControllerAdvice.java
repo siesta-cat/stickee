542dd5648cbc516a76a2ea88d41ef5571007832b
Sugui <anaisusg@gmail.com>
2024-08-16 11:14:46 +0200

    Added handler for NoResourceFoundException

--- src/main/java/cat/siesta/stickee/controller/AppControllerAdvice.java
+++ src/main/java/cat/siesta/stickee/controller/AppControllerAdvice.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @ControllerAdvice
 public class AppControllerAdvice {
@@ -13,6 +14,13 @@ public class AppControllerAdvice {
         return ResponseEntity
                 .status(e.getStatusCode())
                 .body(e.getBody().getDetail());
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<String> handle(NoResourceFoundException e) {
+        return ResponseEntity
+                .status(e.getStatusCode())
+                .body("404 not found");
     }
 
     @ExceptionHandler(Exception.class)
