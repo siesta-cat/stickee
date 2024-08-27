e75455b0085f052db3cbc4a9b253644339357861
Sugui <anaisusg@gmail.com>
2024-08-27 14:55:05 +0200

    Added test for Note.withId()

---
+++ src/test/java/cat/siesta/stickee/unit/NoteTest.java
@@ -0,0 +1,22 @@
+package cat.siesta.stickee.unit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+
+import cat.siesta.stickee.persistence.Note;
+
+public class NoteTest {
+
+    @Test
+    void shouldCreateSameNoteWithGivenId() {
+        var note = new Note(null, "text", LocalDateTime.now());
+        var noteWithId = note.withId("123");
+
+        assertEquals(note.getText(), note.getText());
+        assertEquals(note.getCreationTimestamp(), note.getCreationTimestamp());
+        assertEquals(noteWithId.getId().get(), "123");
+    }
+}
