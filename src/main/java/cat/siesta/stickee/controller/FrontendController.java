cbd794dbe665e13b1ab9cd6f2a98254ad91fe40c
Sugui <anaisusg@gmail.com>
2024-08-29 14:35:11 +0200

    Added note max time to frontend

--- src/main/java/cat/siesta/stickee/controller/FrontendController.java
+++ src/main/java/cat/siesta/stickee/controller/FrontendController.java
@@ -1,5 +1,6 @@
 package cat.siesta.stickee.controller;
 
+import org.apache.commons.lang3.time.DurationFormatUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +17,8 @@ public class FrontendController {
 	@GetMapping("/")
 	public String home(Model model) {
 		model.addAttribute("notesBasePath", stickeeConfig.getNotesBasePath());
+		model.addAttribute("noteMaxAge",
+				DurationFormatUtils.formatDurationWords(stickeeConfig.getNoteMaxAge().toMillis(), true, true));
 		return "index";
 	}
 
