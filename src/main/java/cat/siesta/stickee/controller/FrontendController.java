09d9b51119cd0753c4e2f9d69a083283d72852eb
Sugui <anaisusg@gmail.com>
2024-09-03 18:41:49 +0200

    Added version on index.html. Closes #36

--- src/main/java/cat/siesta/stickee/controller/FrontendController.java
+++ src/main/java/cat/siesta/stickee/controller/FrontendController.java
@@ -2,6 +2,7 @@ package cat.siesta.stickee.controller;
 
 import org.apache.commons.lang3.time.DurationFormatUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,11 +15,15 @@ public class FrontendController {
 	@Autowired
 	StickeeConfig stickeeConfig;
 
+	@Value("${stickee.version}")
+	private String version;
+
 	@GetMapping("/")
 	public String home(Model model) {
 		model.addAttribute("notesBasePath", stickeeConfig.getBasePath());
 		model.addAttribute("notesMaxAge",
 				DurationFormatUtils.formatDurationWords(stickeeConfig.getMaxAge().toMillis(), true, true));
+		model.addAttribute("version", version);
 		return "index";
 	}
 
