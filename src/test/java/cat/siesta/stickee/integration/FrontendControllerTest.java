3ba9274d00aea94a8b87a2cf27304de5bdf0beee
Sugui <anaisusg@gmail.com>
2024-09-21 08:49:02 +0200

    feat(enhanced-view): added enhanced view, hidden for now

--- src/test/java/cat/siesta/stickee/integration/FrontendControllerTest.java
+++ src/test/java/cat/siesta/stickee/integration/FrontendControllerTest.java
@@ -3,12 +3,16 @@ package cat.siesta.stickee.integration;
 import static io.restassured.RestAssured.given;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 
+import cat.siesta.stickee.domain.NoteId;
+import cat.siesta.stickee.service.NoteService;
+import cat.siesta.stickee.utils.NoteStub;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import jakarta.annotation.PostConstruct;
@@ -16,6 +20,9 @@ import jakarta.annotation.PostConstruct;
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class FrontendControllerTest {
+
+    @Autowired
+    NoteService noteService;
 
     @LocalServerPort
     Integer port;
@@ -26,23 +33,38 @@ public class FrontendControllerTest {
     }
 
     @Test
-    public void testHome() throws Exception {
+    void testHome() {
         given().get("/").then().assertThat()
                 .statusCode(HttpStatus.OK.value())
                 .contentType(ContentType.HTML);
     }
 
     @Test
-    public void testCss() throws Exception {
+    void testCss() {
         given().get("/main.css").then().assertThat()
                 .statusCode(HttpStatus.OK.value())
                 .contentType("text/css");
     }
 
     @Test
-    public void testJs() throws Exception {
+    void testJs() {
         given().get("/color-modes.js").then().assertThat()
                 .statusCode(HttpStatus.OK.value())
                 .contentType("text/javascript");
+    }
+
+    @Test
+    void shouldGetDetailedView() {
+        var id = noteService.create(NoteStub.builder().build()).getMaybeId().get();
+        given().get("/notes/detail/" + id).then().assertThat()
+                .statusCode(HttpStatus.OK.value())
+                .contentType(ContentType.HTML);
+    }
+
+    @Test
+    void shouldNotFoundNonExistingNote() {
+        given().get("/notes/detail/" + NoteId.generate()).then().assertThat()
+                .statusCode(HttpStatus.NOT_FOUND.value())
+                .contentType(ContentType.HTML);
     }
 }
