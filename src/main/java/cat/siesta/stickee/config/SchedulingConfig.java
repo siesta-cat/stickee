354f1140c3ef5fcb0ed2db636cd23b95d915419f
Sugui <anaisusg@gmail.com>
2024-09-28 19:24:01 +0200

    chore: add comment

--- src/main/java/cat/siesta/stickee/config/SchedulingConfig.java
+++ src/main/java/cat/siesta/stickee/config/SchedulingConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * This configuration disables scheduling on the test profile.
+ * There are scheduled tasks that can produce unexpected results on tests.
+ */
 @Configuration
 @EnableScheduling
 @Profile("!test")
